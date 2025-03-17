1e7b2eedaf1ef00f83a72fcfc95dd888b245d76a
Julien Rousselot <jujuf50@live.fr>
2025-03-17 19:43:39 +0100

    Insertion Cards dans app

---
+++ src/app/Card/cards.components.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'cards',
+  styleUrl: './cards.components.scss',
+  templateUrl: './cards.components.html',
+})
+export class Cards {
+  title = 'Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+
+  test(){
+    console.log('test');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, input } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { Cards } from './Card/cards.components';
 
+//Soit templateUrl avec url de la route ex: './Card/cards.components.html'
+//Soit template avec le code html ou export
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [Cards],
+  template: '<cards/>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TestAngular';
+  title = 'TestAngulars';
 }
